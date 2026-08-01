e529cb1141e889fe2381995e89779fa3c05b0beb
nian0122 <3048586944@qq.com>
2026-08-01 21:45:52 +0800

    修复漫画列表缓存反序列化 ClassCastException：缓存纯数据 DTO 而非 IPage

--- api-service/src/main/java/com/comicatlas/api/comic/service/ComicListQueryService.java
+++ api-service/src/main/java/com/comicatlas/api/comic/service/ComicListQueryService.java
@@ -1,10 +1,14 @@
 package com.comicatlas.api.comic.service;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.comicatlas.api.comic.dto.ComicListPage;
 import com.comicatlas.api.comic.dto.ComicListQuery;
 import com.comicatlas.api.comic.dto.ComicListVO;
 
 public interface ComicListQueryService {
 
     IPage<ComicListVO> listComics(ComicListQuery query);
+
+    /** 查询一页漫画并返回纯数据分页载体（供缓存层使用）。 */
+    ComicListPage loadPage(ComicListQuery query);
 }
