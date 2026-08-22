3eee626150a2665c3f85f271c4d6a183a47136e4
nian0122 <3048586944@qq.com>
2026-08-22 17:23:57 +0800

    修复 Worker 未使用导入

--- worker-service/src/main/java/com/comicatlas/worker/importer/archive/SevenZipArchiveReader.java
+++ worker-service/src/main/java/com/comicatlas/worker/importer/archive/SevenZipArchiveReader.java
@@ -5,11 +5,8 @@ import com.comicatlas.worker.shared.process.ExternalProcessRunner;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Duration;
