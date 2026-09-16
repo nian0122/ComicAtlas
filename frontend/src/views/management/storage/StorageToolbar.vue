9a082a663bc8593cc26e27a1cbafa95c81fedba3
nian0122 <3048586944@qq.com>
2026-09-16 23:46:41 +0800

    统一输入选择器单一边框

--- frontend/src/views/management/storage/StorageToolbar.vue
+++ frontend/src/views/management/storage/StorageToolbar.vue
@@ -243,6 +243,6 @@ onMounted(() => {
 }
 .filter-bar :deep(.el-select__wrapper.is-focused),
 .filter-bar :deep(.el-input__wrapper.is-focus) {
-  box-shadow: 0 0 0 1px var(--accent) inset;
+  box-shadow: none !important;
 }
 </style>
